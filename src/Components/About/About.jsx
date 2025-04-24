7e98cc13b370f16bd4380f300e0c1a0e7a7d2d57
Abduvohid <abduvohiddotnet@gmail.com>
2025-04-24 20:11:49 +0500

    Updated

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,12 +7,12 @@ const About = () => {
 
 
     return (
-        <section id="about">
+        <section id="about" className="pt-24">
             <div className="container">
-                <div className="h-screen">
+                <div>
                     <h2 className="dark:text-[#111827] w-full mt-0 mx-auto mb-[42px] max-w-[1200px] text-[#F7F3F3] text-5xl font-bold font-code ">My Experience</h2>
-                    <div className="dark:text-[#898989] cursor-pointer font-sans px-[20px] py-[30px] max-w-[1200px] font-bold text-[#abadb2] text-[24px] mb-8 gap-[20px] justify-between flex flex-wrap items-strech">
-                        <ul>
+                    <div className="dark:text-[#898989] cursor-pointer font-sans px-[20px] py-[30px] w-full font-bold text-[#abadb2] text-[24px] mb-8 gap-[20px] justify-between flex flex-wrap items-strech">
+                        <ul className="w-[45%]">
                             <li onClick={() => setItemID(1)}>
                                 <img src="" alt="" />
                                 <div>
@@ -36,16 +36,18 @@ const About = () => {
                             </li>
                             <li></li>
                         </ul>
-                        {experienceData?.map(el => (    
-                            itemID === el?.id ? (
-                                <>
-                                    <div>
-                                        {el.title}
-                                        {el.items[0].description1}
-                                    </div>
-                                </>
-                            ) : ''
-                        ))}
+                        <div className="w-[45%]">
+                            {experienceData?.map(el => (
+                                itemID === el?.id ? (
+                                    <>
+                                        <div>
+                                            {el.title}
+                                            {el.items[0].description1}
+                                        </div>
+                                    </>
+                                ) : ''
+                            ))}
+                        </div>
 
                     </div>
                 </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -20,16 +20,16 @@ const Home = () => {
 
     return (
         <>
-            <section id="home">
-                <div className="dark:bg-[#f7f3f3] bg-[#111827] h-screen flex items-center justify-center">
+            <section id="home" className="pt-24">
+                <div className="dark:bg-[#f7f3f3] bg-[#111827] flex items-center justify-center">
                     <div className="text-center">
                         <h1 className="dark:text-[#111827] w-full mt-0 mx-auto mb-[24px] max-w-[700px] text-[#F7F3F3] text-[60px] font-bold">{HeaderData.MyFISH}</h1>
                         <p className="dark:text-[#898989] font-sans text-justify max-w-[1020px] font-bold text-[#abadb2] text-[24px] mb-8">{HeaderData.subtitle}</p>
-                        <ul className="flex justify-center items-center gap-4">
+                        <ul className="flex justify-center items-center gap-6">
                             {programmLanguages?.map(el => {
                                 return (
                                     <li key={el?.name}>
-                                        <div className="dark:bg-[#E3E8ED] flex items-center bg-[#172135] rounded-[3px] space-x-2">
+                                        <div className="dark:bg-[#E3E8ED] flex items-center bg-[#172135] rounded-[3px] space-x-2 px-2 py-1">
                                             <i>{el?.image}</i>
                                             <p className="dark:text-[#111827] font-sans font-normal leading-[1.537] m-0 text-[#f7f3f3] text-base text-400">{el?.name}</p>
                                         </div>
