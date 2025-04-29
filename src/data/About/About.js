04ab539f22757a448e4b89d5b368a9d4f5a4b944
Abduvohid <abduvohiddotnet@gmail.com>
2025-04-29 15:54:46 +0500

    About section updated

--- src/data/About/About.js
+++ src/data/About/About.js
@@ -6,13 +6,16 @@ const experienceData = [
         to: "Present",
         items: [
             {
-                description1: "Design and implementation of visually appealing and intuitive user interfaces for innovative technology products."
+                sign: ">",
+                description: "Design and implementation of visually appealing and intuitive user interfaces for innovative technology products."
             },
             {
-                description2: "Continuous improvement of the user experience and maintenance of quality standards.",
+                sign: ">",
+                description: "Continuous improvement of the user experience and maintenance of quality standards.",
             },
             {
-                description3: "Conducting testing and validation, user research, and collaborating with other teams to ensure quality and effective integration with the final product."
+                sign: ">",
+                description: "Conducting testing and validation, user research, and collaborating with other teams to ensure quality and effective integration with the final product."
             }
         ]
     },
@@ -23,13 +26,13 @@ const experienceData = [
         to: 2022,
         items: [
             {
-                description1: " Developed and maintained the core web platform, ensuring a seamless and engaging experience for technical bloggers and readers."
+                description: " Developed and maintained the core web platform, ensuring a seamless and engaging experience for technical bloggers and readers."
             },
             {
-                description2: "Collaborated with the design team to create interactive and responsive UI components.",
+                description: "Collaborated with the design team to create interactive and responsive UI components.",
             },
             {
-                description3: "Optimized performance and accessibility to improve user engagement across various devices."
+                description: "Optimized performance and accessibility to improve user engagement across various devices."
             }
         ]
     },
@@ -40,13 +43,13 @@ const experienceData = [
         to: 2020,
         items: [
             {
-                description1: "Engineered immersive VR interfaces and interactive components for a next-generation virtual reality platform."
+                description: "Engineered immersive VR interfaces and interactive components for a next-generation virtual reality platform."
             },
             {
-                description2: "Worked closely with 3D artists and UX designers to create intuitive and futuristic user experiences.",
+                description: "Worked closely with 3D artists and UX designers to create intuitive and futuristic user experiences.",
             },
             {
-                description3: "Implemented performance optimizations to ensure smooth and responsive VR interactions."
+                description: "Implemented performance optimizations to ensure smooth and responsive VR interactions."
             }
         ]
     }
